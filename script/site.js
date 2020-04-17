ef50cda45fa9e52819c09ed2a9ab3aae53c5debf
TurtleSnakeMoose <misharoskin@gmail.com>
2020-04-17 21:58:09 +0300

    major progress and changes.
    super incomplete. but very very basic layout is in place

--- script/site.js
+++ script/site.js
@@ -3,11 +3,20 @@ ip.site = ip.site || {};
 
 $(function (){
 
+	// scrolls to element when clicking on navbar menu item.
+	ip.site.scrollToElement = function (item) {
+		var scrollToElement = $(`.${$(item).data('scrollTo')}`).eq(0);
+		$('html,body').animate({
+            scrollTop: scrollToElement.offset().top
+		}, 500);
+	}
+
+	// prepares and sends the contact info to e-mail by smtp.js
     ip.site.submitForm = function (submitBtn){
 
 		var form = submitBtn.closest('form');
 
-		if(!ip.validate.validateForm(form)){
+		if(!ip.validate.validateForm(form)) {
 			return;
 		}
 
