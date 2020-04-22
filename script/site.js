dd35550cf6f70ae28442542497e2e9a09064cb9f
TurtleSnakeMoose <misharoskin@gmail.com>
2020-04-23 01:32:41 +0300

    MAJOR REDESIGN - unfinished

--- script/site.js
+++ script/site.js
@@ -4,11 +4,8 @@ ip.site = ip.site || {};
 $(function (){
 
 	// scrolls to element when clicking on navbar menu item.
-	ip.site.scrollToElement = function (item) {
-		var scrollToElement = $(`.${$(item).data('scrollTo')}`).eq(0);
-		$('html,body').animate({
-            scrollTop: scrollToElement.offset().top
-		}, 500);
+	ip.site.navigateToLectureSignup = function () {
+		window.open('https://meshulam.co.il/purchase?b=7188235352cc1f3521ee8cc891842e37', '_blank')
 	}
 
 	// prepares and sends the contact info to e-mail by smtp.js
