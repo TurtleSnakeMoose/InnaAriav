973b5012f96e42ccd5c4e7082a8427b59171504e
TurtleSnakeMoose <misharoskin@gmail.com>
2020-04-24 20:21:17 +0300

    site is now responsive
    partially finished

--- script/site.js
+++ script/site.js
@@ -3,6 +3,15 @@ ip.site = ip.site || {};
 
 $(function (){
 
+	// TODO: load all pissible textual context from js func
+	// // this load text from a js funciton, allowing the client to easily modify texts around the page.
+	// ip.site.loadTextualContent = function () { 
+	// 	ip.content.loadContent_aboutMe();
+	// 	ip.content.loadContent_saidAboutMe();
+	// 	ip.content.loadContent_steps();
+	// 	ip.content.loadContent_lectureInfo();
+	// }	
+
 	ip.site.navigateToLectureSignup = function () {
 		window.open('https://meshulam.co.il/purchase?b=7188235352cc1f3521ee8cc891842e37', '_blank')
 	}
